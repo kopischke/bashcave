a5d827f87d38565842e4a58b99a4da53dc1e02cc
Martin Kopischke <martin@kopischke.net>
2013-04-10 01:23:17 +0200

    Default to fetching global locale
    If all else fails, take the long route.

--- system.sh
+++ system.sh
@@ -4,11 +4,11 @@ function get_global_locale {
 
 function get_locale {
 	local locale
-	# get cached value if requested and present
-	[[ $1 == '--cached' && -f "$2" ]] && locale=$(while read -r; do echo "$REPLY"; break; done < "$2")
-	# get the defaults value if there is no cached value present, or if none has been requested
-	[[ -z "$locale" ]] && locale=$(get_global_locale)
-	# asynchronously refresh cache (if requested)
-	[[ $1 == '--cached' ]] && { { mkdir -p "${2%/*}"; get_global_locale > "$2"; } & disown; }
-	echo "$locale"
+	# get cached value if requested and present, refresh asynchronously
+	if [[ $1 == '--cached' ]]; then
+		[[ -f "$2" ]] && locale=$(while read -r; do echo "$REPLY"; break; done < "$2")
+		{ { mkdir -p "${2%/*}"; get_global_locale > "$2"; } & disown; } 2>/dev/null
+	fi
+	# get the defaults value otherwise
+	[[ -n "$locale" ]] && echo "$locale" || get_global_locale
 }
