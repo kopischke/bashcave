df9ec0ea612646c5f8c608f142f07653444f1556
Martin Kopischke <martin@kopischke.net>
2013-04-16 23:21:38 +0200

    Even faster cheetah
    trusting in bash error ahndling. Alos: comments brought in line with
    rest of bashcave.

--- core/io.sh
+++ core/io.sh
@@ -1,6 +1,9 @@
 # IO.SH LIBRARY: HANDLE FILES ELEGANTLY
-# Echoes the contents of a file to stdout.
-function cheetah { # a faster cat ... for short bursts
-	[[ -f "$1" && -r "$1" ]] || return 1
+# Echoes the contents of FILE to stdout.
+# Usage: cheetah FILE
+#
+# NB: cheetah is faster than cat for small files, but slower for very large ones,
+# hence its name: a faster cat ... on short ranges.
+function cheetah {
 	while read -r || [[ -n "$REPLY" ]]; do echo "$REPLY"; done < "$1"
 }
